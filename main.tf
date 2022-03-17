a4f8b4d0e614e5c3c00f086a8a9ff396a4ec498b
Ilya Kohanovskiy <i.kohanvs@gmail.com>
2022-03-17 15:19:44 +0400

    change aws resopitory url

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "circleci_context" "accounts-app-context" {
 resource "circleci_context_environment_variable" "accounts-app-context-env" {
   for_each = {
     AWS_RESOURCE_NAME_PREFIX = "${var.AWS_RESOURCE_NAME_PREFIX}"
-    AWS_ECR_REPOSITORY_URL = "${module.app_infrastructure.aws_ecr_repository.repository_url}"
+    AWS_ECR_REPOSITORY_URL = "${module.app_infrastructure.aws_ecr_repository.registry_id}.dkr.ecr.${var.AWS_REGION}.amazonaws.com"
     AWS_ECR_REPOSITORY_REGISTRY_ID = "${module.app_infrastructure.aws_ecr_repository.registry_id}"
   }
 

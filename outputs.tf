6564c42260f03c07a4dcd31517bfb97e89975b8c
Ilya Kohanovskiy <i.kohanvs@gmail.com>
2022-03-17 13:09:08 +0400

    add aws_ecr_repository_url to outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "load_balancer_ip" {
   value = data.terraform_remote_state.main_infrastructure.outputs.load_balancer_ip
 }
+
+output "aws_ecr_repository_url" {
+  value = module.main-infrastructure.aws_ecr_repository_url
+}
